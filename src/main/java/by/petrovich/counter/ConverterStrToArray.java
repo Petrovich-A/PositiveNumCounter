436d29b83b400e1d515e4381153661d1e5f70412
Petrovich-A <A.Piatrovich@gmail.com>
2023-03-20 15:19:51 +0300

    handled NumberFormatException

--- src/main/java/by/petrovich/counter/ConverterStrToArray.java
+++ src/main/java/by/petrovich/counter/ConverterStrToArray.java
@@ -1,11 +1,22 @@
 package by.petrovich.counter;
 
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ConverterStrToArray {
-    public int[] convertStrToArray(String numbersWithSpaces){
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public int[] convertStrToArray(String numbersWithSpaces) {
         String[] numbersArray = numbersWithSpaces.split(" ");
         int[] numbers = new int[numbersArray.length];
         for (int i = 0; i < numbersArray.length; i++) {
-            numbers[i] = Integer.parseInt(numbersArray[i]);
+            try {
+                numbers[i] = Integer.parseInt(numbersArray[i]);
+            } catch (NumberFormatException e) {
+                LOGGER.log(Level.ERROR, "Int value in String");
+            }
         }
         return numbers;
     }
